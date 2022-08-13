7cc9d8db48b5062e6c51c2262cb77caece95e716
jzhang2018 <joyzhang386@gmail.com>
2022-08-13 17:16:06 -0400

    vanilla dom

---
+++ scripts/preview.js
@@ -0,0 +1,11 @@
+//document on load
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    //get board element
+    const board = document.querySelector('#board');
+
+    //add a static message
+    board.innerHTML = 'Grow a beautiful tree';
+
+    //build board content with graphql schema
+});
