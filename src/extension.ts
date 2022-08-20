22f2cb76c9841cff4e2db7dbac449ca3cbddded2
SteveFutureSWE <84763472+SteveFutureSWE@users.noreply.github.com>
2022-08-20 14:28:17 -0400

    solved initial steps of vscode suggestion

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,140 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { type } from 'os';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "surfql" is now active!');
+let queryIntiate = true
+let queryLevel : any
+interface PokeQuery {
+	pokemon: {
+		name:string,
+		type: string,
+		moves: string
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('surfql.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SurfQL!');
+	}
+}
+export function activate(context: vscode.ExtensionContext) {
+
+	//each provider is a set of rules, for what needs to be typed and what will be suggested
+
+	const provider1 = vscode.languages.registerCompletionItemProvider('javascript', {
+
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+			// a simple completion item which inserts `Hello World!`
+			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+			// a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which will be
+			// honored by the editor.
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			const docs : any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
+			snippetCompletion.documentation = docs;
+			docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
+
+			// a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+
+
+	
+
+
+			//definitions
+			// CompletionItem is text that's suggested by VSCode to the user...
+			//example: if you define completion item as chicken and type "chi" it will show "chicken"
+
+			//commitCharacters is the string that triggers the ???
+			
+
+
+
+			//We need the completion item to basically be the next item in the query
+
+			const commitCharacterCompletion = new vscode.CompletionItem('chicken');
+			commitCharacterCompletion.commitCharacters = ['``'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			// a completion item that retriggers IntelliSense when being accepted,
+			// the `command`-property is set which the editor will execute after 
+			// completion has been inserted. Also, the `insertText` is set so that 
+			// a space is inserted after `new`
+			const commandCompletion = new vscode.CompletionItem('new');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'new ';
+			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+			// return all completion items as array
+			return [
+				simpleCompletion,
+				snippetCompletion,
+				commitCharacterCompletion,
+				commandCompletion
+			];
+		}
 	});
 
-	context.subscriptions.push(disposable);
-}
+	const provider2 = vscode.languages.registerCompletionItemProvider(
+		'javascript',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+				const pokeQuery: any = {
+					pokemon: {
+						name: "Pikachu",
+						type: "Electric",
+						moves: "Tackle"
+					}
+				}
+	
+
+				//Initial activation should be via back tick, but ALL further queries should NOT be using this
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.includes("`")) {
+					return undefined;
+				}
+
+
+				if (queryIntiate == true) {
+					let objArr = Object.keys(pokeQuery)
+				let suggestions: Array<any> = []
+
+				objArr.forEach(e => {
+					suggestions.push(new vscode.CompletionItem(e + ": {", vscode.CompletionItemKind.Method))
+					console.log(suggestions)
+					queryIntiate = false
+					queryLevel = e // == "pokemon"	
+					
+				})
+				return suggestions;
+				} else {
+					let objArr = Object.keys(pokeQuery[queryLevel])
+
+					let suggestions: Array<any> = []
+
+				objArr.forEach(e => {
+					suggestions.push(new vscode.CompletionItem(e + ": {", vscode.CompletionItemKind.Method))
+					console.log(suggestions)
+					queryIntiate = false
+					queryLevel = e 
+
+					
+					
+				})
+
+				return suggestions;
+				}
+
+				
+
+				
+			}
+		},
+		'`' // triggered whenever a backtick is being typed
+	);
+
+	context.subscriptions.push(provider1, provider2);
+}
