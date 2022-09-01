c8978981420d79d6c93d14714bbc8c128aeea4b2
ethanmcrae <ethanmcrae0@gmail.com>
2022-09-01 15:12:12 -0600

    Changed default autofill to support multi-line queries

--- src/extension.ts
+++ src/extension.ts
@@ -8,43 +8,36 @@ import * as path from 'path';
 import * as fs from 'fs';
 import parser from "./parser";
 
-let queryIntiate = true;
-let queryLevel : any;
-
-	let schema: any = {
-		pokemon: {
-			name: "Pikachu",
-			type: {
-				electric: {
-					shocking: true,
-					treasureChest: {
-						treasure: 'yarr'
-					}
-				},
-				water: false
+let schema: any = {
+	pokemon: {
+		name: "Pikachu",
+		type: {
+			electric: {
+				shocking: true,
+				treasureChest: {
+					treasure: 'yarr'
+				}
 			},
-			moves: "Tackle"
+			water: false
 		},
-		test2: {
-			tester: 'test',
-		},
-		test3: {
-			works: true
-		}
-	};
-
+		moves: "Tackle"
+	},
+	test2: {
+		tester: 'test',
+	},
+	test3: {
+		works: true
+	}
+};
 
 let history: any[] = [];
-let level = 0;
-
-
-let characters: string[] = ['`','{'];
+const triggerCharacters: string[] = ['`', '{'];
+const indentation = '  '; // TODO: Update based off the config (spacing/tab amount)
 
 export function activate(context: vscode.ExtensionContext) {
-
-	 // Use the console to output diagnostic information (console.log) and errors (console.error)
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "surfql" is now active!');
+  console.log('SurfQL is now active 🌊');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -55,7 +48,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage("Hello World from SurfQL!");
   });
 
-  //let's do a poptup for preview Schema
+  //let's do a popup for preview Schema
   let previewSchema = vscode.commands.registerCommand(
     "surfql.previewSchema",
     async () => {
@@ -78,11 +71,11 @@ export function activate(context: vscode.ExtensionContext) {
         }
       });
 
-      //create a newpanel in webView
+      //create a new panel in webView
       const panel = vscode.window.createWebviewPanel(
-        "Preview Schema", //viewType, internal use
-        "Schema Preview", //Preview title in the tag
-        vscode.ViewColumn.Beside, //where the new panel shows
+        "Preview Schema", // viewType, internal use
+        "Schema Preview", // Preview title in the tag
+        vscode.ViewColumn.Beside, // where the new panel shows
         {
           enableScripts: true,
         } //option to add scripts
@@ -130,15 +123,9 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(previewSchema);
 
-	//this function accepts the name of the level that's being clicked and suggests the next level
-	let levelChecker = vscode.commands.registerCommand('surfql.levelChecker', async (queryText) => {
-		console.log(queryText);
-		queryLevel = queryText;
-	});
-
-	//each provider is a set of rules, for what needs to be typed and what will be suggested
-
-	const provider1: vscode.Disposable = vscode.languages.registerCompletionItemProvider('javascript', {
+	
+	// Each provider is a set of rules, for what needs to be typed, to create suggestions
+	const exampleProvider: vscode.Disposable = vscode.languages.registerCompletionItemProvider('javascript', {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
@@ -158,18 +145,9 @@ export function activate(context: vscode.ExtensionContext) {
 			// the `commitCharacters`-property is set which means that the completion will
 			// be inserted and then the character will be typed.
 
-
-	
-
-
 			//definitions
 			// CompletionItem is text that's suggested by VSCode to the user...
-			//example: if you define completion item as chicken and type "chi" it will show "chicken"
-
-			//commitCharacters is the string that triggers the ???
-			
-
-
+			// - example: if you define completion item as chicken and type "chi" it will show "chicken"
 
 			//We need the completion item to basically be the next item in the query
 
@@ -195,163 +173,141 @@ export function activate(context: vscode.ExtensionContext) {
 			];
 		}
 	});
-	let provider2: vscode.Disposable = vscode.languages.registerCompletionItemProvider(
+
+	const suggestionProvider: vscode.Disposable = vscode.languages.registerCompletionItemProvider(
 		'javascript',
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-				
-			
-				//Initial activation should be via back tick, but ALL further queries should NOT be using this
 				const linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.includes("`")) {
-					return undefined;
-				}
+				const triggerCharacter = linePrefix.at(-1);
+				console.log('Trigger Character:', triggerCharacter);
 
-				function setLevel(e: any) {
-					console.log('the function has been activated and the level is ', e);
-				}
-
-				const suggestions: Array<any> = [];
-
+				// Removed to enable multi-line query auto-fill
+				// TODO: Break early when it can be determined that the cursor is not within a query string
+				// if (!linePrefix.includes("`")) {
+				// 	return undefined;
+				// }
+				
 				//Step 1: analyze what has been typed by the user, between the backticks
 				// ex: Pokequery -> if what the user has typed == obj.keys
 				//  - Register pokequery as our main object
 				//  - Or... schemaFile: { Dishes:{} Order:{} ...{} }
 				//    - For example: Pokequery is now comparable to schemaFile.
 				//    - We will always use our schemaFile as our root (thats what we initialize with all the sub schemas)
-
-				// let query = `Pokemon { {} }`
-				// ['Pokemon','mo'] --> return nothing
-				// ['Pokemon'] --> moves
-
-
-				// history.push(queryLevel); [Pokemon, Moves] <-- this now happens in the event listener
-				console.log('we are on level', level);
-				let objArr = traverseObject(schema, history);
+				
+				const suggestions: Array<any> = [];
+				const objArr = traverseObject(schema, history);
 
 				objArr.forEach(e => {
-					let tempCompItem = new vscode.CompletionItem(e + " ", vscode.CompletionItemKind.Keyword);
-					tempCompItem.command = { command: 'surfql.levelChecker', title: 'Re-trigger completions...', arguments: [e] };
+					let tempCompItem = new vscode.CompletionItem(e + ' (SurfQL)', vscode.CompletionItemKind.Keyword); // What is displayed
+					tempCompItem.insertText = new vscode.SnippetString('\n' + indentation + e + '${0}\n'); // What is added
+					// tempCompItem.command = { command: 'surfql.levelChecker', title: 'Re-trigger completions...', arguments: [e] };
 					suggestions.push(tempCompItem);
-					
-					//console.log(suggestions);
-					queryIntiate = false;
-					queryLevel = e ;
 				});
 				
-				level++;
 				return suggestions;
 			}
 		},
-		...characters // Trigger characters
+		...triggerCharacters
 	);
 
-	context.subscriptions.push(provider1, provider2, levelChecker);
+	context.subscriptions.push(exampleProvider, suggestionProvider);
 
 	//step 0: test
-// - Do we need provider? And do we need to subscribe the event listener?
-// - Can we log the current line on EVERY change?
+	// - Do we need provider? And do we need to subscribe the event listener?
+	// - Can we log the current line on EVERY change?
 
-//step 1: Parse the current query and determine where the user is
+	//step 1: Parse the current query and determine where the user is
 
-//we need to create an event listener of some sort 
-//it needs to read the current text 
+	//we need to create an event listener of some sort 
+	//it needs to read the current text 
 
-// "pokemon { type {} }"
-// parse the above into our history array: ['pokemon', 'type']
-// this needs to be replaced every single iteration of a letter typed/backspaced
+	// "pokemon { type {} }"
+	// parse the above into our history array: ['pokemon', 'type']
+	// this needs to be replaced every single iteration of a letter typed/backspaced
 
-// if(pokequery[history[0]]) -> iterate -> pokequery[typ] --> undefined []
+	// if(pokequery[history[0]]) -> iterate -> pokequery[typ] --> undefined []
 
-//step 2: Develop function that will determine the appropriate query to suggest 
-//once the user has backspaced enough
-// e.g: Pokemon {type: {ele }} = no suggestion
-// e.g: Pokemon {type: { }} = suggest electric
+	//step 2: Develop function that will determine the appropriate query to suggest 
+	//once the user has backspaced enough
+	// e.g: Pokemon {type: {ele }} = no suggestion
+	// e.g: Pokemon {type: { }} = suggest electric
 
-//step 3: Add logic for multi-line queries
+	//step 3: Add logic for multi-line queries
 
 
 
-vscode.workspace.onDidChangeTextDocument((e) => {
-	// console.log(line);
-	const lineNumber: number = e.contentChanges[0].range.start.line;
-	const characterNumber: number = e.contentChanges[0].range.start.character;
-	const line: string = e.document.lineAt(lineNumber).text;
-	console.log('the line number is', lineNumber);
-	console.log('the character number is', characterNumber);
-	//console.log('the position is', new vscode.Position(lineNumber, 20));
-	e.document.positionAt;
-	
-	// figure out way after determing cursor position
-	// to navigate left and right until hitting backtick `
-	/*
+	vscode.workspace.onDidChangeTextDocument((e) => {
+		const lineNumber: number = e.contentChanges[0].range.start.line;
+		const characterNumber: number = e.contentChanges[0].range.start.character;
+		const line: string = e.document.lineAt(lineNumber).text;
+		console.log('row', lineNumber, 'column', characterNumber);
+		e.document.positionAt;
+		
+		// figure out way after determing cursor position
+		// to navigate left and right until hitting backtick `
+		/*
 
-	['pokemon','type','moves'] pokemon.moves
-		const query = `
-			pokeQuery {
-				pokemon {
-					type {
-						(how to suggest here)
-					}
-					moves {
-						if you see a closing bracket
-						ignore the next opening bracket moving bkwrds
-					}
-					nest3 {
+		['pokemon','type','moves'] pokemon.moves
+			const query = `
+				pokeQuery {
+					pokemon {
+						type {
+							(how to suggest here)
+						}
+						moves {
+							if you see a closing bracket
+							ignore the next opening bracket moving bkwrds
+						}
+						nest3 {
 
+						}
 					}
 				}
-			}
-		`
-	*/
+			`
+		*/
 
-	currentQuery(lineNumber, characterNumber);
+		currentQuery(lineNumber, characterNumber);
 
-  /**
-   * Parses the document returning query information
-   * @param lineNumber lists the VSCode line [index 0] the user is on
-   * @param cursorLocation a number representing the cursor location
-   * @return nothing but re-declares history
-   */
-	function currentQuery(lineNumber: number, cursorLocation:number): void {
-		let lineHistory: string[] = [];
-		let line: string = e.document.lineAt(lineNumber).text;
-    // Cut off everything after the cursor
-		line = line.slice(0, cursorLocation + 1);
+		/**
+		 * Parses the document returning query information
+		 * @param lineNumber lists the VSCode line [index 0] the user is on
+		 * @param cursorLocation a number representing the cursor location
+		 * @return nothing but re-declares history
+		 */
+		function currentQuery(lineNumber: number, cursorLocation:number): void {
+			let lineHistory: string[] = [];
+			let line: string = e.document.lineAt(lineNumber).text;
+			// Cut off everything after the cursor
+			line = line.slice(0, cursorLocation + 1);
 
-    // Iterate through the lines of the file (starting from the cursor moving to the start of the file)
-		while(lineNumber >= 0) {
-			// When the start of the query was found: This is the last loop
-			if(line.includes('`')) {
-				lineNumber = -1; // Set line number to -1 to end the loop
-				// Slice at the backtick
-				const startOfQueryIndex = line.indexOf('`');
-				line = line.slice(startOfQueryIndex+1);
+			// Iterate through the lines of the file (starting from the cursor moving to the start of the file)
+			while(lineNumber >= 0) {
+				// When the start of the query was found: This is the last loop
+				if(line.includes('`')) {
+					lineNumber = -1; // Set line number to -1 to end the loop
+					// Slice at the backtick
+					const startOfQueryIndex = line.indexOf('`');
+					line = line.slice(startOfQueryIndex+1);
+				}
+				
+				lineHistory.push(...line.split(/\s+/g).reverse());
+				lineNumber--;
+				if (lineNumber >= 0) {
+					line = e.document.lineAt(lineNumber).text;
+				}
 			}
 			
-			lineHistory.push(...line.split(/\s+/g).reverse());
-			lineNumber--;
-			if (lineNumber >= 0) {
-				line = e.document.lineAt(lineNumber).text;
-			}
+			//const result = words.filter(word => word.length > 6);
+			
+			// Clean up the parsed query array into a useable history array
+			lineHistory = lineHistory.filter((str) => str); // Filter out the empty strings from the query array
+			lineHistory.reverse(); // The nested order is opposite from how it is typed
+			console.log('the line is', line);
+			history = lineHistory.map((str) => str.replace('{', '')); // Clean up the opening brackets | ex: ['{name'] or ['{']
+			history = history.filter((str) => str); // Filter out the empty strings from the history array
+			console.log('History:', history.join(' -> ') || 'empty...');
 		}
-    
-		//const result = words.filter(word => word.length > 6);
-		
-    // Clean up the parsed query array into a useable history array
-    lineHistory = lineHistory.filter(characters => characters); // Filter out the empty strings from the query array
-		lineHistory.reverse(); // The nested order is opposite from how it is typed
-		console.log('the previous history is', lineHistory);
-		console.log('the line is', line);
-		history = lineHistory.map((str) => str.replace('{', '')); // Clean up the opening brackets | ex: ['{name'] or ['{']
-		history = history.filter((str) => str); // Filter out the empty strings from the history array
-		console.log('the current history is now', history);
-	}
-
-	//parse through text and create array element everytime for every word
-	// - Find start/end of query (trim line string)
-	// 		- notes: it tracks backticks + curly braces
-	// - Parse
 	});
 };
 
@@ -389,7 +345,12 @@ const getWebViewContent = (scriptSrc: String, styleSrc: String) => {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-// Traverse to the current part of the object based on the history
+/**
+ * Traverses an object to return the properties/keys at a given level
+ * @param obj Traditionally: the parsed query file.
+ * @param history An array representing the traversal path for the obj.
+ * @returns The properties/keys at the end of the traversed object.
+ */
 function traverseObject(obj: any, history: string[]): string[] {
 	// If our obj isn't an object we have hit the end of our traversal
 	if (typeof obj !== 'object') {
@@ -404,18 +365,26 @@ function traverseObject(obj: any, history: string[]): string[] {
   return traverseObject(obj[history[0]], history.slice(1));
 };
 
-
-
-//Out-of-scope features pre-presentation
-// Live-share compatibility (usability)
-// ability to detect ONLY graphql query vs parsing the whole document (efficiency)
-// splash site 
-// vscode publication
-// check to see if the cursor is even within a query
-// When the suggestion is another nested object show brackets. But when its an endpoint don't show brackets.
-
-//Question for the GQL experts
-// - Are dollar signs $ ever used in GQL?
+/**
+ * Given a line from a file, a string is returned representing the indentation
+ * in spaces.
+ * @param line 
+ * @param add Will result in an increase/decrease of returned spacing.
+ * @return The amount of spaces representing the indentation
+ */
+ const lineIndentation = (line: string, add: number = 0): string => {
+  // TODO: Update this function to work with tabs as well
+  let indentation = 0; // Initialize a counter
+  for (const char of line) { // Iterate through each character of the line
+    if (char !== ' ') { // If the character is not a space:
+      return ' '.repeat(indentation + add); // Return the indentation amount
+    } else { // If the character is a space:
+      indentation++; // increment the indentation amount
+    }
+  }
+  // In the case where the entire line is filled with spaces:
+  return ' '.repeat(indentation + add); // Return the indentation amount
+};
 
 //modify the returned schemaObj
 function createNestedObj(obj: any) {
@@ -431,3 +400,14 @@ function createNestedObj(obj: any) {
     };
     return obj;
 }
+
+//Out-of-scope features pre-presentation
+// Live-share compatibility (usability)
+// ability to detect ONLY graphql query vs parsing the whole document (efficiency)
+// splash site 
+// vscode publication
+// check to see if the cursor is even within a query
+// When the suggestion is another nested object show brackets. But when its an endpoint don't show brackets.
+
+//Question for the GQL experts
+// Are dollar signs $ ever used in GQL?
